c4f2edc118cb6d6362531c738e902d36958fcbed
Chris Creighton <creighcl6510@gmail.com>
2019-04-23 09:10:13 -0400

    4-24 items

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadSceneById(int sceneId)
+    {
+        SceneManager.LoadScene(sceneId);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            LoadSceneById(0);
+        }
+    }
+}
