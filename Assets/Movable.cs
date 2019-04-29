e1592cf5f624fa65a26f70fd9a24637153bc4170
Chris Creighton <creighcl6510@gmail.com>
2019-04-29 18:08:09 -0400

    ugh middle of something

---
+++ Assets/Movable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movable : MonoBehaviour
+{
+    [SerializeField] float _movementSpeed = .4f;
+    [SerializeField] Vector2 _movementDirection = Vector2.down;
+    Rigidbody2D _rb2d;
+
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();   
+    }
+
+    void Update()
+    {
+        Debug.Log(_rb2d.velocity.y + " " + _movementSpeed);
+        if (_rb2d.velocity.x < (_movementDirection.x * _movementSpeed) || _rb2d.velocity.y > (_movementDirection.y * _movementSpeed))
+        {
+            _rb2d.AddForce(_movementDirection * (_movementSpeed * 25) * Time.deltaTime, ForceMode2D.Force);
+        } else
+        {
+            Debug.Log("THROTTLE");
+        }
+    }
+}
